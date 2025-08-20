be9ca6631749875643b4919b94cfaa607ecc9d82
ayushsahu <ayushsahu97810@gmail.com>
2025-08-20 11:13:26 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,9 @@ const App = () => {
 
 
    const tabsData = [
-    { title: "Tab1", content: "This is the content for Tab1." },
-    { title: "Tab2", content: "This is the content for Tab2." },
-    { title: "Tab3", content: "This is the content for Tab3." },
+    { title: "Tab 1", content: "This is the content for Tab 1." },
+    { title: "Tab 2", content: "This is the content for Tab 2." },
+    { title: "Tab 3", content: "This is the content for Tab 3." },
   ];
   return (
     <div>
